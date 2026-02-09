fbe4b83c5604d527a93bd8df5d2f7e54af3943d5
FutsalDex <futsaldex@gmail.com>
2026-02-09 12:22:19 +0000

    fix: unificar next.config y añadir firebase storage a remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
-
-import type {NextConfig} from 'next';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* Ignoramos errores para permitir el despliegue */
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -32,6 +31,12 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'i.ibb.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
         port: '',
         pathname: '/**',
       },
